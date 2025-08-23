ab3ca0aad7f26c2ec3f661ed4a8f61bf098c1331
fabi0619 <faroan.2004@gmail.com>
2025-08-23 17:05:37 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://fabi0619.github.io',
   integrations: [mdx(), sitemap()],
 
   vite: {
